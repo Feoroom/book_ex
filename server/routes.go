0bb2ce5a075b8b954224aa9d8c71759fb35a5da8
Feoroom <74305987+Feoroom@users.noreply.github.com>
2024-03-18 15:54:35 +0300

    add some logic in handlers

--- server/routes.go
+++ server/routes.go
@@ -4,9 +4,11 @@ import "net/http"
 
 func Routes(mux *http.ServeMux, db Database) {
 
+	fileServer := http.FileServer(http.Dir("./server/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/list", db.List)
 	mux.HandleFunc("/price", db.Price)
 	mux.HandleFunc("/", Home)
-	mux.HandleFunc("/update", db.Update)
 	mux.HandleFunc("/add", db.Add)
 }
